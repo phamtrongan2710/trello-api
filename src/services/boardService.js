481b87f4494ae4b0a21cbb2c14c9a134b0523613
An <phamtrongan2710@gmail.com>
2025-02-04 22:55:57 +0700

    api get board detail

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -7,6 +7,8 @@
 
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   try {
@@ -36,6 +38,17 @@ const createNew = async (reqBody) => {
   } catch (error) { throw error }
 }
 
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+    return board
+  } catch (error) { throw error }
+}
+
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
