a9566817dbb06d7390325f1c6217a7f20a8b558d
An <phamtrongan2710@gmail.com>
2025-02-14 19:58:44 +0700

    update cors: not allow localhost

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,14 +6,12 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    // Update mới: Ở video số 75 trong chuỗi MERN Stack PRO khi chúng ta deploy dự án lên một Server Production thì sẽ sửa lại đoạn này thêm một chút nữa để phù hợp với từng môi trường production hoặc dev nhé. Học với mình thì các bạn cứ yên tâm về sự chỉn chu chuẩn chỉnh nhé :D
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // Môi trường dev
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
 
-    // Kiểm tra dem origin có phải là domain được chấp nhận hay không
+    // Môi trường production
     if (WHITELIST_DOMAINS.includes(origin)) {
       return callback(null, true)
     }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domains được phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // Không cần localhost nữa vì ở file config/cors luôn cho phép môi trường dev
+  // 'http://localhost:5173'
 ]
 
 export const BOARD_TYPES = {
