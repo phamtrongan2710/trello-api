1bdc0c5e27746a0dfcd248df746eef32d05a2500
An <phamtrongan2710@gmail.com>
2025-02-16 15:44:56 +0700

    update white domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 export const WHITELIST_DOMAINS = [
   // Không cần localhost nữa vì ở file config/cors luôn cho phép môi trường dev
   // 'http://localhost:5173'
-  'https://trello-web-ans-projects-48c871b1.vercel.app'
+  'https://trello-web-ashy.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
